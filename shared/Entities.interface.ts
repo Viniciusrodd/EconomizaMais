93462aed494ed226377fa088df575c55a4f7478c
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-13 16:21:59 -0300

    shared - entities interfaces

---
+++ shared/Entities.interface.ts
@@ -0,0 +1,79 @@
+
+// User
+export interface User {
+   id: string; // UUID
+   name: string;
+   residenceName?: string;
+   numberOfResidents?: number;
+   createdAt: Date;
+   updatedAt: Date;
+};
+
+
+// Tariff
+export interface Tariff {
+   id: string; // UUID
+   userId: string;
+   energyTariff: number;
+   waterTariff: number;
+   gasTariff: number;
+   createdAt: Date;
+   updatedAt: Date;
+};
+
+
+// Monthly Consumption
+export interface MonthlyConsumption {
+   id: string; // UUID
+   userId: string;
+   year: number;   // ex: 2025
+   month: number;  // 1–12
+   energyKwh: number;
+   waterM3: number;
+   gasM3: number;
+   createdAt: Date;
+   updatedAt: Date;
+};
+
+
+// Simulation
+export type SimulationTargetType =
+   | 'energy'
+   | 'water'
+   | 'gas'
+   | 'all';
+
+export interface Simulation {
+   id: string; // UUID
+   userId: string;
+   reductionPercent: number;
+   targetType: SimulationTargetType;
+   calculatedSaving: number;
+   calculatedEnvironmentalImpact: string;
+   createdAt: Date;
+};
+
+
+// AI Insight
+export type AIInsightCategory =
+   | 'dicas'
+   | 'padroes'
+   | 'anomalias';
+
+export interface AIInsight {
+   id: string; // UUID
+   userId: string;
+   inputSummary: string;
+   aiResponse: string;
+   insightCategory: AIInsightCategory;
+   createdAt: Date;
+};
+
+
+// Export History
+export interface ExportHistory {
+   id: string; // UUID
+   userId: string;
+   filePath: string;
+   exportedAt: Date;
+};
