655a87cf0553e27623d9be6fd0e0372f9729910e
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-14 14:52:24 -0300

    backend/database-setup - simulation sequelize model

---
+++ backend/src/infra/sequelize/models/Simulations.model.ts
@@ -0,0 +1,66 @@
+
+// imports
+import { DataTypes, Model } from "sequelize";
+
+// import connection
+import { ConnectionDatabase } from "@config/database";
+
+// import interfaces
+import type { Simulation } from '@interfaces/Entities.interface';
+import type { SimulationTargetType } from "@interfaces/Entities.interface";
+
+
+// class - simulation model
+class SimulationModel extends Model<Simulation> {
+   public id!: string;
+   public userId!: string;
+   public reductionPercent!: number;
+   public targetType!: SimulationTargetType;
+   public calculatedSaving!: number;
+   public calculatedEnvironmentalImpact!: string;
+   public createdAt!: Date;
+};
+
+
+// mapping
+SimulationModel.init({
+   id: {
+      type: DataTypes.CHAR(36),
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+   },
+   userId: {
+      type: DataTypes.CHAR(36),
+      allowNull: false
+   },
+   reductionPercent: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false
+   },
+   targetType: {
+      type: DataTypes.ENUM('energy', 'water', 'gas', 'all'),
+      allowNull: false
+   },
+   calculatedSaving: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false
+   },
+   calculatedEnvironmentalImpact: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+   },
+   createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+   }
+}, {
+   sequelize: ConnectionDatabase,
+   modelName: 'Simulation',
+   timestamps: true,
+   tableName: 'Simulations'
+});
+
+
+export default SimulationModel;
