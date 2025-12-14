881f3805cf6171f9c2075b8d97134d4839a1b2cd
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-14 11:20:59 -0300

    backend/server-setup - security middlewares config

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,6 +1,10 @@
 
 // imports
-import { Application } from "express";
+import { Application, json, urlencoded } from "express";
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
 
 // import env
 import dotenv from 'dotenv';
@@ -21,6 +25,21 @@ export class Server {
    // start methods
    public start(): void {
       this.startServer(this.app);
+      this.securityMiddlewares(this.app);
+   };
+
+
+   // security middlewares
+   private securityMiddlewares(app: Application): void {
+      app.use(hpp()); // prevents against "HTTP Parameter Pollution"
+      app.use(helmet()); // safety config HTTP headers
+      app.use(cors({
+         origin: process.env.CLIENT_URL,
+         credentials: true,
+         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }));
+
+      console.log('✔️ Security middlewares');
    };
 
 
@@ -28,7 +47,7 @@ export class Server {
    private startServer(app: Application): void {
       app.listen(process.env.SERVER_PORT, () =>{
          console.log(
-            '✔️ Server running at port: ', 
+            '✔️ Server: ', 
             process.env.SERVER_PORT
          );
       });
