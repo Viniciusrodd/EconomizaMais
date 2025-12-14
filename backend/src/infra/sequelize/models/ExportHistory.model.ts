2c75a203fe1c568dd9089f2d550bb8f806aaadec
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-14 15:52:14 -0300

    backend/database-setup - export history sequelize model

---
+++ backend/src/infra/sequelize/models/ExportHistory.model.ts
@@ -0,0 +1,50 @@
+
+// imports
+import { DataTypes, Model } from "sequelize";
+
+// import connection
+import { ConnectionDatabase } from "@config/database";
+
+// import interfaces
+import type { ExportHistory } from '@interfaces/Entities.interface';
+
+
+// class - export history model
+class ExportHistoryModel extends Model<ExportHistory> {
+   public id!: string;
+   public userId!: string;
+   public filePath!: string;
+   public exportedAt!: Date;
+};
+
+
+// mapping
+ExportHistoryModel.init({
+   id: {
+      type: DataTypes.CHAR(36),
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+   },
+   userId: {
+      type: DataTypes.CHAR(36),
+      allowNull: false,
+   },
+   filePath: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+   },
+   exportedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+   }
+}, {
+   sequelize: ConnectionDatabase,
+   modelName: 'ExportHistory',
+   timestamps: true,
+   tableName: 'ExportHistorys'
+});
+
+
+export default ExportHistoryModel;
