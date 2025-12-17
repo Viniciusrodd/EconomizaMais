07f9c5b19a3e0f17399ba29d695282c9ba8583fe
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-17 13:02:05 -0300

    backend/tariffs - tariffs creation service

--- backend/src/domain/services/Tariffs.service.ts
+++ backend/src/domain/services/Tariffs.service.ts
@@ -15,6 +15,30 @@ import { models } from "@root/infra/sequelize/Relations";
 class TariffService {
 
    // create tariff - public
+   public async createTariff(
+      tariffData: CreateTariffDTO
+   ): Promise<TariffResponseDTO> {
+      // validations
+      const { energy_tariff, water_tariff, gas_tariff } = tariffData;
+      if(!energy_tariff || !water_tariff || !gas_tariff){
+         throw new Error('❌ All tariff fields are required');
+      }
+
+      // check existing tariffs
+      const existingTariffCount = await models.TariffModel.count();
+      if(existingTariffCount > 0){
+         throw new Error('❌ Only one tariffs is allowed per installation');
+      }
+
+      // tariff DB creation
+      const tariffs = await models.TariffModel.create({
+         energy_tariff,
+         water_tariff,
+         gas_tariff
+      });
+
+      return tariffs;
+   };
 
 
    // get tariff - public
--- shared/interfaces/Entities.interface.ts
+++ shared/interfaces/Entities.interface.ts
@@ -12,8 +12,8 @@ export interface User {
 
 // Tariff
 export interface Tariff {
-   id: string; // UUID
-   user_id: string;
+   id?: string; // UUID
+   user_id?: string;
    energy_tariff: number;
    water_tariff: number;
    gas_tariff: number;
