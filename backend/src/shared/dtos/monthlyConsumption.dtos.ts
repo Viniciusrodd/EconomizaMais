5a2416622f519100570b99d5b071813dde2c557b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-18 10:23:18 -0300

    backend/monthly_cons. - monthly_cons. DTOs

---
+++ backend/src/shared/dtos/monthlyConsumption.dtos.ts
@@ -0,0 +1,39 @@
+
+// create monthly_consumption DTO
+export interface CreateMonthlyConsumptionDTO {
+   userId: string;
+   year: number;
+   month: number;
+   energy_kwh: number;
+   water_m3: number;
+   gas_m3: number;
+};
+
+
+// update monthly_consumption DTO
+export interface UpdateMonthlyConsumptionDTO {
+   energy_kwh?: number;
+   water_m3?: number;
+   gas_m3?: number;
+};
+
+
+// get monthly_consumption DTO
+export interface MonthlyConsumptionResponseDTO {
+   id: string;
+   year: number;
+   month: number;
+   energy_kwh: number;
+   water_m3: number;
+   gas_m3: number;
+   created_at: Date;
+   updated_at: Date;
+};
+
+
+// get monthly_consumption summary DTO
+export interface MonthlyConsumptionSummaryDTO {
+   average_energy: number;
+   highest_energy_month: number;
+   variation_last_month: number;
+};
