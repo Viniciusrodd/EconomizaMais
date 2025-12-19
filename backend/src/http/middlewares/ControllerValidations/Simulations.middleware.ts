f5f4fdb032601cb378dae57c91596e6edf30cc4a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-19 11:44:20 -0300

    backend/simulations - create simulations validation

---
+++ backend/src/http/middlewares/ControllerValidations/Simulations.middleware.ts
@@ -0,0 +1,29 @@
+
+// imports
+import { body, param, ValidationChain } from 'express-validator';
+
+
+// simulations creation
+const createSimulationsValidation = (): ValidationChain[] => {
+   return [
+      // invalid target type
+      body('target_type')
+         .exists().withMessage('Tipo de simulação é obrigatório')
+         .isString().withMessage('Tipo de simulação apenas em characteres')
+         .isIn([
+            'energy', 'water', 'gas', 'all' 
+         ]).withMessage('Tipo de simulação deve ser apenas: energia, água, gás ou tudo'),
+      
+      // invalid reduction percent   
+      body('reduction_percent')
+         .exists().withMessage('Porcentagem de redução é obrigatório')
+         .isFloat({ min: 0 }).withMessage('Porcentagem de redução deve ser no minímo 0')
+         .toFloat(),
+   ];
+};
+
+
+// export validations
+export const simulationsValidations = {
+   createSimulationsValidation
+};
