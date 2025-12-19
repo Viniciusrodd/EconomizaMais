e08dff72729d167117ea757e0c2c05cdfcfffdf3
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-19 13:07:30 -0300

    backend/simulations - simulations entity - issues resolve

--- backend/src/domain/entities/Simulations.entity.ts
+++ backend/src/domain/entities/Simulations.entity.ts
@@ -1,7 +1,6 @@
 
 // types
 type TargetTypes = 'energy' | 'water' | 'gas' | 'all';
-type Consume = 'energy_kwh' | 'water_m3' | 'gas_m3';
 
 // interfaces
 interface iTypes {
@@ -10,95 +9,61 @@ interface iTypes {
    gas: number;
 };
 
-// import entities
-import { MonthlyConsumptionHistoryEntity } from "./MonthlyConsumptionHistory.entity";
-
-// import services
-import { monthlyConsumptionService } from "@services/MonthlyConsumption.service";
-
-// import interfaces
-import { MonthlyConsumption } from "@interfaces/Entities.interface";
-
-
 
 // class - Simulations entities
 export class SimulationsEntities {
    // properties
    private readonly targetType: TargetTypes;
    private readonly reductionPercent: number;
+   private readonly averageConsumption: iTypes;
    private readonly tariffs: iTypes;
 
    // constructor
    constructor(
       targetType: TargetTypes,
       reductionPercent: number,
+      averageConsumption: iTypes, 
       tariffs: iTypes
    ) {
+      // validations
+      if (reductionPercent <= 0 || reductionPercent > 100) {
+         throw new Error('Reduction percent must be between 0 and 100');
+      }
+
       this.targetType = targetType;
       this.reductionPercent = reductionPercent;
+      this.averageConsumption = averageConsumption; 
       this.tariffs = tariffs
    };
 
 
-   
-   //// private methods
-
-
-   // get consumptions
-   private async getConsumptions(): Promise<MonthlyConsumption[]> {
-      return await monthlyConsumptionService.getAllMonthConsService();
-   };
-   
-
-   // avarage consume
-   private async avarageConsume(consume: Consume): Promise<number> {
-      // get conumptions - private
-      const consumptions: MonthlyConsumption[] = await this.getConsumptions();
-      
-      // monthlyConsumptionHistory instance - get avarage consume
-      const monthlyConsumptionHistory: MonthlyConsumptionHistoryEntity = new MonthlyConsumptionHistoryEntity(consumptions);
-      const avarage = monthlyConsumptionHistory.buildSummary(consume);
-      return avarage.average_consume;
-   };
-
-
-
-   ////
-
-   
-
    // calculate Monthly Saving
-   public async calculateMonthlySaving(): Promise<number> {
+   public calculateMonthlySaving(): number {
       // saving controll
       let saving = 0;
       
       // "energy" or "all"
       if (this.targetType === 'energy' || this.targetType === 'all') {
-         const avarageConsume = await this.avarageConsume('energy_kwh');
 
          saving +=
-         avarageConsume *
+         this.averageConsumption.energy * 
          this.tariffs.energy *
          (this.reductionPercent / 100);
       }
 
       // "water" or "all"
       if (this.targetType === 'water' || this.targetType === 'all') {
-         const avarageConsume = await this.avarageConsume('water_m3');
-         
          saving +=
-         avarageConsume *
-         this.tariffs.water *
+         this.averageConsumption.energy * 
+         this.tariffs.energy *
          (this.reductionPercent / 100);
       }
 
       // "gas" or "all"
       if (this.targetType === 'gas' || this.targetType === 'all') {
-         const avarageConsume = await this.avarageConsume('gas_m3');
-
          saving +=
-         avarageConsume *
-         this.tariffs.gas *
+         this.averageConsumption.energy * 
+         this.tariffs.energy *
          (this.reductionPercent / 100);
       }
 
@@ -107,8 +72,7 @@ export class SimulationsEntities {
 
 
    // calculate Annual Saving
-   public async calculateAnnualSaving(): Promise<number> {
-      const annualSaving = await this.calculateMonthlySaving() * 12
-      return Number((annualSaving).toFixed(2));
+   public calculateAnnualSaving(): number {
+      return Number((this.calculateMonthlySaving() * 12).toFixed(2));
    };
 }
