254d6ec8a148305c333c4b12a22f448c6ef27ff1
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-19 11:00:34 -0300

    backend/server - IA model request service

---
+++ backend/src/domain/services/AiServices/AI.service.ts
@@ -0,0 +1,47 @@
+
+// imports
+import axios from "axios";
+
+// import env
+import dotenv from 'dotenv';
+dotenv.config({});
+
+// import utils
+import { getErrorMessage } from "@utils/ErrorHandler.util";
+
+
+// class - AI service
+class AiService {
+
+   public async modelRequest(
+      prompt: string,
+      model: string
+   ): Promise<string> {
+      // validations
+      if(!prompt) throw new Error('Prompt for AI model request is necessary');
+      if(!model) throw new Error('Model is a mandatory field');
+
+      try{
+         // get model response
+         const modelResponse = await axios.post(process.env.OLLAMA_URL as string, {
+            'model': model,
+            'prompt': prompt,
+            'stream': false
+         }, { timeout: 5000 });
+
+         // get result
+         const result = typeof modelResponse.data === 'string'
+            ? modelResponse.data
+            : modelResponse.data.response;
+         if(!result) throw new Error('Empty response from AI model');
+            
+         return result.trim();
+      }
+      catch(error){
+         console.error('[AIService] Error while requesting local AI model:', getErrorMessage(error));
+         throw new Error('Failed to process AI model request');
+      }
+   };
+
+};
+export const aiService: AiService = new AiService();
