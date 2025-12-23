032a6c83b1ee5c39625278e608a7b02810b11180
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-23 12:00:06 -0300

    backend/tests - userController / create user tests

---
+++ backend/src/tests/UserController.test.ts
@@ -0,0 +1,81 @@
+
+// import controller
+import { userController } from "@controllers/User.controller";
+
+// import services
+import { userService } from "@services/User.service";
+
+// import mocks
+import { userMockRequest, userMockResponse } from "@mocks/user.mock";
+
+
+// utils
+const id: string = 'uuid-123';
+const name: string = 'teste nome';
+const residence_name: string = 'casa teste';
+const number_of_residents: number = 3;
+const created_at: Date = new Date(Date.now());
+const updated_at: Date = new Date(Date.now());
+const fakeUser = { id, name, residence_name, number_of_residents, created_at, updated_at };
+
+
+// mocks
+jest.mock('@services/User.service');
+
+
+describe('UserController', () => {
+
+   // before each tests
+   beforeEach(() =>{
+      jest.resetAllMocks(); // reset mock's states, like calls, implementations...
+   });
+
+
+   // create user
+   describe('createUser', () => {
+
+      // 201
+      it('Should return 201 and created user', async () => {
+         // request / response
+         const req = userMockRequest({ name, residence_name, number_of_residents }) as any;
+         const res = userMockResponse();
+
+         // spy functions
+         jest.spyOn(userService, 'createUserService').mockResolvedValue(fakeUser);
+      
+         // controller method
+         await userController.createUser(req, res);
+
+         // expects
+         expect(res.status).toHaveBeenCalledWith(201);
+         expect(res.json).toHaveBeenCalledWith({
+            success: true,
+            message: '✔️ User successfully created',
+            data: fakeUser
+         });
+      });
+
+      // 500
+      it('Should return 500 if service throws error', async () => {
+         // request / response
+         const req = userMockRequest({ name, residence_name, number_of_residents }) as any;
+         const res = userMockResponse();
+
+         // spy functions
+         const consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+         jest.spyOn(userService, 'createUserService').mockRejectedValue(new Error('Service error (test)'));
+
+         // controller method
+         await userController.createUser(req, res);
+
+         // expects
+         expect(res.status).toHaveBeenCalledWith(500);
+
+         // restore mock
+         consoleSpy.mockRestore();
+      });
+
+   });
+
+
+});
