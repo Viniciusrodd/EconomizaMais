41394f444c0581bf0f295a533a1bd7b42ee7cda5
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-23 15:53:25 -0300

    backend/tests - simulationsController / simulations mock

---
+++ backend/src/mocks/simulations.mock.ts
@@ -0,0 +1,29 @@
+
+// imports
+import { Response } from "express";
+
+// import DTOs
+import { 
+   CreateSimulationDTO,
+} from "@DTOs/Simulations.dtos";
+
+
+// request mock
+export const simulationsMockRequest = (
+   body: CreateSimulationDTO | {},
+   params?: string | {}
+) => ({ // immediately returns a literal object
+   body,
+   params
+});
+
+
+// response mock
+export const simulationsMockResponse = (): Response => {
+   const res: Response = {} as Response; // empty object "pretending" being a Response
+
+   res.status = jest.fn().mockReturnValue(res); // "fn()" its a "simulate function"
+   res.json = jest.fn().mockReturnValue(res);
+   
+   return res;
+};
