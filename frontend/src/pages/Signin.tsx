b50e9e0a0ba43495797a58cfa21edb2646d61d46
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-12-31 14:47:17 -0300

    frontend/pages - sign in modal

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,14 +1,12 @@
 
 // import css
 import styles from '@styles/pages/Signin.module.css';
+
 // import images
 import desc_img from '@images/signin/desc_img.jpg';
 import welcome_img from '@images/signin/signin.png';
 
-
-/*
 // import hooks
-import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 // import interfaces
@@ -16,19 +14,62 @@ import type { iModalConfig } from '@interfeces/Modal.interface';
 
 // import components
 import Modal from '@components/Modal';
-*/
+
+
 
 // sign in
 const SignIn = () => {
+   //// variables
+   const [ modal_display, setModal_display ] = useState<boolean>(false);
+   const [ modal_title, setModal_title ] = useState<string>('');
+   const [ modal_msg, setModal_msg ] = useState<string>('');
+   const [ modal_btt, setmodal_btt ] = useState<boolean | string>(false);
+   const [ modal_btt_2, setModal_btt_2 ] = useState<boolean | string>(false);
+
+
+   //// functions
+
+
+   // modal config
+   const modal_config = ({ title, msg, btt1, btt2, display }: iModalConfig) => {
+      setModal_title(title ?? '');
+      setModal_msg(msg ?? '');
+      setmodal_btt(btt1 ?? false);
+      setModal_btt_2(btt2 ?? false);
+      setModal_display(display ?? false);
+   };   
+
+   // close modal
+   const closeModal = () =>{
+      modal_config({
+         title: '', msg: '', btt1: false, 
+         btt2: false, display: false
+      });
+   };
+
+
+   //// jsx   
+
+
    return (
       <div className={ styles.container }>
+         { /* modal */ }
+         <Modal 
+            title={ modal_title }
+            msg={ modal_msg }
+            btt1={ modal_btt }
+            btt2={ modal_btt_2 }
+            display={ modal_display }
+            onClose={ closeModal }
+         />
+
          <div className={ styles.desc_img }>
             <img src={ desc_img } alt="money_img" />
 
             <div className={ styles.desc_background }>
                <h1>ECONOMIZA+</h1>
                <p>
-                  Bem-vindo(a) ao Economiza+ — sua nova ferramenta para <strong>consumo consciente</strong> e <strong>economia doméstica!</strong> <br />
+                  Bem-vindo(a) ao <strong>Economiza+</strong>, sua nova ferramenta para <strong>consumo consciente</strong> e <strong>economia doméstica!</strong> <br />
                   Aqui, você pode registrar e acompanhar o consumo de energia, água e gás da sua casa de forma 
                   simples e visual. 
                </p>
