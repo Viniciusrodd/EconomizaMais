8d45642a7e7a7a74d084fc96c334c088f6bf320d
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-01-01 10:59:01 -0300

    frontend/contexts - loading context creation

---
+++ frontend/src/contexts/Loading/Loading.provider.tsx
@@ -0,0 +1,25 @@
+
+// import hooks
+import { useState, type ReactNode } from "react";
+
+// import context
+import { LoadingContext } from '@contexts/Loading/Loading.context';
+
+
+// types
+type LoadingPropsProvider = {
+   children: ReactNode
+};
+
+
+// provider
+export const LoadingProvider = ({ children }: LoadingPropsProvider) =>{
+   // states
+   const [ loading, setLoading ] = useState<boolean>(false);
+
+   return (
+      <LoadingContext.Provider value={{ loading, setLoading }}>
+         { children }
+      </LoadingContext.Provider>
+   )
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// import providers
+import { LoadingProvider } from '@contexts/Loading/Loading.provider.tsx'
+
 
 // render
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      <App />
+      <LoadingProvider>
+
+         <App />
+      
+      </LoadingProvider>
    </StrictMode>,
 );
