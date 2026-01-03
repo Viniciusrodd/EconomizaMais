67f67fd63a21018117ed718da1c32fdfe835f036
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-01-03 09:36:40 -0300

    frontend/pages - homepage modal implementation

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -5,12 +5,61 @@ import styles from '@styles/pages/Homepage.module.css';
 // import components
 import Navbar from '@components/Navbar';
 import Sidebar from '@components/Sidebar';
+import Modal from '@components/Modal';
+
+// import interfaces
+import type { iModalConfig } from '@interfeces/frontend/Modal.interface';
+
+// import hooks
+import { useState } from 'react';
 
 
 // homepage
 const Homepage = () => {
+   //// variables
+   const [ modal_display, setModal_display ] = useState<boolean>(false);
+   const [ modal_title, setModal_title ] = useState<string>('');
+   const [ modal_msg, setModal_msg ] = useState<string>('');
+   const [ modal_btt, setmodal_btt ] = useState<boolean | string>(false);
+   const [ modal_btt_2, setModal_btt_2 ] = useState<boolean | string>(false);
+
+
+   //// functions
+
+
+   // modal config
+   const modal_config = ({ title, msg, btt1, btt2, display }: iModalConfig) => {
+      setModal_title(title ?? '');
+      setModal_msg(msg ?? '');
+      setmodal_btt(btt1 ?? false);
+      setModal_btt_2(btt2 ?? false);
+      setModal_display(display ?? false);
+   };   
+
+   // close modal
+   const closeModal = () =>{
+      modal_config({
+         title: '', msg: '', btt1: false, 
+         btt2: false, display: false
+      });
+   };
+
+
+   //// jsx
+
+
    return (
       <div className={ styles.container }>
+         { /* modal */ }
+         <Modal 
+            title={ modal_title }
+            msg={ modal_msg }
+            btt1={ modal_btt }
+            btt2={ modal_btt_2 }
+            display={ modal_display }
+            onClose={ closeModal }
+         />     
+     
          {/* navbar */}
          <Navbar />
 
