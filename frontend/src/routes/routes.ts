d3dc2c7a1c27bdadfcc2ca9254c044f04bfd1645
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-01-03 09:54:55 -0300

    frontend/services - tariffs service

--- frontend/src/routes/routes.ts
+++ frontend/src/routes/routes.ts
@@ -2,3 +2,21 @@
 
 // user routes
 export const userRoute: string = 'http://localhost:5115/api/users/user';
+
+// tariffs routes
+export const tariffRoutes: string = 'http://localhost:5115/api/tariffs/tariff';
+
+// monthlyConsumptions routes
+export const monthConsRoutes: string = 'http://localhost:5115/api/monthCons/monthly-consumption';
+
+// simulations routes
+export const simulationsRoutes: string = 'http://localhost:5115/api/simulations/simulation';
+
+// ai Insights routes
+export const aiInsightsRoutes: string = 'http://localhost:5115/api/aiInsights/insight';
+export const aiInsightsRoutesGet: string = 'http://localhost:5115/api/aiInsights/insights';
+
+// simulations routes
+export const historyRoutesCreate: string = 'http://localhost:5115/api/export/pdf';
+export const historyRoutesGet: string = 'http://localhost:5115/api/export/histories';
+export const historyRoutesDownload: string = 'http://localhost:5115/api/export/history';
---
+++ frontend/src/services/Tariffs.service.ts
@@ -0,0 +1,68 @@
+
+// imports
+import axios from "axios";
+
+// import DTOs
+import type { 
+   CreateTariffDTO,
+   TariffResponseDTO,
+   UpdateTariffDTO
+} from '@DTOs/Tariffs.dtos';
+
+// import routes
+import { tariffRoutes } from "@routes/routes";
+
+
+// tariff service - frontend
+class TariffService {
+
+   // create tariff
+   public async createTariffService(
+      data: CreateTariffDTO
+   ): Promise<TariffResponseDTO> {
+      try{
+         const res = await axios.post(tariffRoutes, data);
+         return res.data.data;
+      }
+      catch(error){
+         if(axios.isAxiosError(error)){
+            throw error.response?.data?.data || error.response?.data?.message;
+         }
+         throw error;
+      }
+   };
+
+
+   // get tariffs
+   public async getTariffService(): Promise<TariffResponseDTO> {
+      try{
+         const res = await axios.get(tariffRoutes);
+         return res.data.data;
+      }
+      catch(error){
+         if(axios.isAxiosError(error)){
+            throw error.response?.data?.data || error.response?.data?.message;
+         }
+         throw error;
+      }
+   };
+
+
+   // update tariff
+   public async updateTariffService(
+      data: UpdateTariffDTO
+   ): Promise<TariffResponseDTO> {
+      try{
+         const res = await axios.put(tariffRoutes, data);
+         return res.data.data;
+      }
+      catch(error){
+         if(axios.isAxiosError(error)){
+            throw error.response?.data?.data || error.response?.data?.message;
+         }
+         throw error;
+      }
+   };
+
+};
+export const tariffService: TariffService = new TariffService();
