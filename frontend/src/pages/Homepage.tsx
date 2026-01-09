bb4504b388c8e4b920549ef316030fd8098baa8a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-01-09 15:35:20 -0300

    frontend/pages - homepage monthConsumptions summary

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -16,8 +16,10 @@ import Modal from '@components/Modal';
 // import interfaces
 import type { iModalConfig } from '@interfeces/frontend/Modal.interface';
 import type { 
-   MonthlyConsumptionResponseDTO
+   MonthlyConsumptionResponseDTO,
+   MonthlyConsumptionSummaryDTO
 } from '@DTOs/monthlyConsumption.dtos';
+import type { TariffResponseDTO } from '@DTOs/Tariffs.dtos';
 
 // import hooks
 import { useState, useEffect, useContext } from 'react';
@@ -42,10 +44,11 @@ const Homepage = () => {
    const [ modal_btt, setmodal_btt ] = useState<boolean | string>(false);
    const [ modal_btt_2, setModal_btt_2 ] = useState<boolean | string>(false);
    const [ modal_event, setModal_event ] = useState<string>('');   
-   const [ energy_tariff, setEnergy_tariff ] = useState<number>(0);
-   const [ water_tariff, setWater_tariff ] = useState<number>(0);
-   const [ gas_tariff, setGas_tariff ] = useState<number>(0);
+   const [ tariffs, setTariffs ] = useState<TariffResponseDTO>();
    const [ monthConsList, setMonthConsList ] = useState<MonthlyConsumptionResponseDTO[]>([]);
+   const [ energySummary, setEnergySummary ] = useState<MonthlyConsumptionSummaryDTO>();
+   const [ waterSummary, setWaterSummary ] = useState<MonthlyConsumptionSummaryDTO>();
+   const [ gasSummary, setGasSummary ] = useState<MonthlyConsumptionSummaryDTO>();
 
 
    //// contexts
@@ -80,22 +83,15 @@ const Homepage = () => {
 
    // fetch tariffs
    const fetchTariffs = async () => {
-      setLoading(true);
-
       const response = await tariffService.getTariffService();
       if(!response) throw new Error('❌ Erro ao achar tarifas');
 
       // set tariffs
-      setEnergy_tariff(response.energy_tariff);
-      setWater_tariff(response.water_tariff);
-      setGas_tariff(response.gas_tariff);
-      setLoading(false);
+      setTariffs(response);
    };
 
    // fetch monthly consumptions
    const fetchMonthCons = async () => {
-      setLoading(true);
-
       const response = await monthlyConsumptionService.getMonthConsService();
       if(!response) throw new Error('❌ Erro ao achar meses de consumo');
 
@@ -110,11 +106,33 @@ const Homepage = () => {
       setMonthConsList(sortedResponse);
    };
 
-   // check tariffs + month cons
+   // fetch monthly consumptions summary
+   const fetchMonthConsSummary = async () => {
+      // energy
+      const energy_response = await monthlyConsumptionService.getMonthConsSummaryService('energy_kwh');
+      if(!energy_response) throw new Error('❌ Erro ao achar resumo de consumos de energia');
+      setEnergySummary(energy_response);
+
+      // water
+      const water_response = await monthlyConsumptionService.getMonthConsSummaryService('water_m3');
+      if(!water_response) throw new Error('❌ Erro ao achar resumo de consumos de água');
+      setWaterSummary(water_response);
+
+      // gas
+      const gas_response = await monthlyConsumptionService.getMonthConsSummaryService('gas_m3');
+      if(!gas_response) throw new Error('❌ Erro ao achar resumo de consumos de gás');
+      setGasSummary(gas_response);
+   };
+
+   // check datas
    useEffect(() => {
+      // tariffs
       const getTariffs = async () => {
+         setLoading(true);
+
          try{
             await fetchTariffs();
+            setLoading(false);
          }
          catch(error){
             console.error('❌ Error at check tariffs: ', error);
@@ -127,12 +145,18 @@ const Homepage = () => {
                msg: `${ error }! \n Registre agora suas Tarifas de consumo`, 
                btt1: 'Registrar', btt2: false, display: true
             });
+
+            setLoading(false);
          }
       };
 
+      // monthly consumptions
       const getMonthCons = async () => {
+         setLoading(true);
+
          try{
             await fetchMonthCons();
+            setLoading(false);
          }
          catch(error){
             console.error('❌ Error at check monthly consumptions: ', error);
@@ -145,11 +169,35 @@ const Homepage = () => {
                msg: `${ error }! \n Registre agora seu mês de consumo`, 
                btt1: 'Registrar', btt2: false, display: true
             });
+
+            setLoading(false);
+         }
+      };
+
+      // monthly consumptions summary
+      const getMonthConsSummary = async () => {
+         setLoading(true);
+
+         try{
+            await fetchMonthConsSummary();
+            setLoading(false);
+         }
+         catch(error){
+            console.error('❌ Error at check monthly consumptions summary: ', error);
+            
+            modal_config({
+               title: 'Erro ❌', 
+               msg: `${ error }`, 
+               btt1: false, btt2: 'Tentar novamente', display: true
+            });
+
+            setLoading(false);
          }
       };
 
       getTariffs();
       getMonthCons();
+      getMonthConsSummary();
    }, []);
 
 
@@ -178,7 +226,17 @@ const Homepage = () => {
             
             <div className={ styles.data_container }>
                <div className={ styles.data }>
+                  <p>{ tariffs?.energy_tariff }</p>
+                  <p>{ tariffs?.water_tariff }</p>
+                  <p>{ tariffs?.gas_tariff }</p>
 
+                  { monthConsList && monthConsList.map(month => (
+                     <p>{ month.month } - { month.year }</p>
+                  )) }
+
+                  <p>{ energySummary?.average_consume }</p>
+                  <p>{ waterSummary?.average_consume }</p>
+                  <p>{ gasSummary?.average_consume }</p>
                </div>
             </div>
          </div>
